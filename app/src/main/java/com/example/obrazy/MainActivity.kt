ddabbb2ff13390d8efa070898ba5778d880fbf85
Stanisław Frąk <S.Frak@student.zschie.edu.pl>
2021-11-07 15:29:51 +0100

    Zmiana Obrazu Dziala

--- app/src/main/java/com/example/obrazy/MainActivity.kt
+++ app/src/main/java/com/example/obrazy/MainActivity.kt
@@ -13,12 +13,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        val CheckWidok: CheckBox =findViewById(R.id.CheckWidoczny);
-        val Obrazek: ImageView =findViewById(R.id.Obrazek);
+        val CheckWidok: CheckBox =findViewById(R.id.CheckWidoczny)
+        val Obrazek: ImageView =findViewById(R.id.Obrazek)
 
-        val Obracacz: EditText =findViewById(R.id.Obrot);
-        val SkalaX: EditText =findViewById(R.id.SkalaX);
-        val SkalaY: EditText =findViewById(R.id.SkalaY);
+        val Obracacz: EditText =findViewById(R.id.Obrot)
+        val SkalaX: EditText =findViewById(R.id.SkalaX)
+        val SkalaY: EditText =findViewById(R.id.SkalaY)
+
+        val ButtonNext: ImageButton =findViewById(R.id.ButtonNext)
+        val ButtonPrev: ImageButton =findViewById(R.id.ButtonPrev)
+        val ButtonClear: ImageButton =findViewById(R.id.ButtonClear)
+        val ButtonCamera: ImageButton =findViewById(R.id.ButtonCamera)
+
+        var KtoryObraz = 1
+
+        fun ZmianaObrazu(){
+            if(KtoryObraz>4){KtoryObraz=4}
+            if(KtoryObraz<1){KtoryObraz=1}
+            if(KtoryObraz==1){Obrazek.setImageResource(R.drawable.pies1)}
+            if(KtoryObraz==2){Obrazek.setImageResource(R.drawable.pies2)}
+            if(KtoryObraz==3){Obrazek.setImageResource(R.drawable.pies3)}
+            if(KtoryObraz==4){Obrazek.setImageResource(R.drawable.pies4)}
+        }
+
+        ButtonNext.setOnClickListener(){
+            KtoryObraz=KtoryObraz+1
+            ZmianaObrazu()
+        }
+        ButtonPrev.setOnClickListener(){
+            KtoryObraz=KtoryObraz-1
+            ZmianaObrazu()
+        }
 
         CheckWidok.setOnClickListener(){
             if(CheckWidok.isChecked()){
